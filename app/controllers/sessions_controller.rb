4335eff6c7b6402dc657953596d04e34183415e7
Avi Zacherman <avizacherman@gmail.com>
2015-08-30 15:14:09 -0400

    Controllers

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,13 +3,24 @@ class SessionsController < ApplicationController
 
 
 def set_session
-	access_token = User.authorize_token(request.env['omniauth.auth']['credentials'])
+	access_token = authorize_token(request.env['omniauth.auth']['credentials'])
 	session[:access_token] = access_token
-	redirect_to request.referer 
+	if URI(request.referer).path == '/' 
+		redirect_to '/events'
+	else
+		redirect_to URI(request.referer).path
+	end
 end
 
 def end_session
 	session[:access_token] = nil
 end
+
+private 
+	 def authorize_token(auth)
+		 	access_token = auth['token']
+	 		return access_token
+ 		end
+
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,3 @@
 class User < ActiveRecord::Base
 	 
-	 def authorize_token(auth)
-		 	access_token = auth['token']
-	 		return access_token
- 		end
 end
