c9a40d06198b4f2120d62dd04e1f9ce5b0684924
Avi Zacherman <avizacherman@gmail.com>
2015-08-30 15:44:55 -0400

    Routing and rolling, login works

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,8 @@ Rails.application.routes.draw do
 	get 'events' => 'events#index'
 	get 'events/:id' => 'events#view'
 
+  get 'auth/facebook', as: 'auth_provider'
+  get 'auth/facebook/callback' => 'sessions#set_session'
 
 	#   Example of named route that can be invoked with purchase_url(id: product.id)
 	#     get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
