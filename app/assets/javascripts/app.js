36ee2730a64d543257291420d8ebd492f17f6d3e
Spencer Denauski <oxideous@hotmail.com>
2015-08-31 16:52:30 -0400

    lots of JS and complete login scheme

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,45 +1,65 @@
-$(document).ready(function () {
+$(document).ready(initialize);
+$(document).on('page:load', initialize);
 
-    var yourLat
-    var yourLng
+function initialize() {
+	
+	var yourLat
+	var yourLng
+	
+	var bKey = 'AIzaSyDXo_-3dpRQz_yvYHP6yEaYUA1_vYlxglM';
 
-    navigator.geolocation.getCurrentPosition(function(pos){
-        yourLat = pos.coords.latitude
-        yourLng = pos.coords.longitude
-    })
+	//GET LOCATION
+	navigator.geolocation.getCurrentPosition(function(pos){
+		yourLat = pos.coords.latitude
+		yourLng = pos.coords.longitude
+	})
+	
+	//CREATES MAP AND CENTERS ON CURRENT LOCATION
+	setTimeout( function(){ 
+		baseMap = new google.maps.Map($('#map')[0], {
+			center: {lat:  yourLat, lng: yourLng},
+			zoom: 15
+		}); 
+		baseMarker = new google.maps.Marker({
+			position: {lat:  yourLat, lng: yourLng},
+			label: "U",
+			map: baseMap
+		})
+	}, 4000)
 
-     setTimeout( function(){ 
-      baseMap = new google.maps.Map($('#map')[0], {
-        center: {lat:  yourLat, lng: yourLng},
-        zoom: 15
-            }); 
-        baseMarker = new google.maps.Marker({
-            position: {lat:  yourLat, lng: yourLng},
-             label: "U",
-            map: baseMap
-        })
-  }, 4000)
+	//PASSES INFO FROM 
+	$(".hasLocation").each(function(){
+		var lat = parseFloat($(this).attr("lat"))
+		var lng = parseFloat($(this).attr("lng"))
+		$(this).on("click", function(){
+			map = new google.maps.Map($('#map')[0], {
+				center: {lat:  lat, lng: lng},
+				zoom: 15
+			}); 
 
-    $(".hasLocation").each(function(){
-       var lat = parseFloat($(this).attr("lat"))
-       var lng = parseFloat($(this).attr("lng"))
-        $(this).on("click", function(){
-        map = new google.maps.Map($('#map')[0], {
-        center: {lat:  lat, lng: lng},
-        zoom: 15
-            }); 
+			marker = new google.maps.Marker({
+				position: {lat:  lat, lng: lng},
+				label: "E",
+				map: map
+			})
+			
+//			$.get('https://maps.googleapis.com/maps/api/directions/json?origin=' + yourLat + ',' + yourLng + '&destination=' + lat + ',' + lng + '&key=' + bKey, function(data){
+//				console.log(data);
+//			});
 
-        marker = new google.maps.Marker({
-            position: {lat:  lat, lng: lng},
-            label: "E",
-            map: map
-            })
-        })
-    }) 
+			var directionsService = new google.maps.DirectionsService;
+			var directionsDisplay = new google.maps.DirectionsRenderer; 
+			directionsDisplay.setMap(map); 
+			
+			calculateAndDisplayRoute(directionsService,directionsDisplay,{lat: yourLat, lng: yourLng}, {lat: lat, lng: lng})
+			
+		})
+	}) 
 
-    verticalMagic();
-});
-//HOMEPAGE CENTERING
+	verticalMagic();
+}
+
+//HOMEPAGE CENTERING - VERTICAL MAGIC
 function verticalMagic() {
     if ($('.vertical-magic') < 1) {
         return;
@@ -62,6 +82,25 @@ function verticalMagic() {
     resizeFunc();
     $(window).resize(function () { resizeFunc(); });
 }
+
+
+//GOOGLE MAPS GET AND DISPLAY ROUTE
+function calculateAndDisplayRoute(directionsService, directionsDisplay, origin, destination) {
+	directionsService.route({
+		origin: origin,
+		destination: destination,
+		travelMode: google.maps.TravelMode.DRIVING
+	}, function(response, status) {
+		if (status === google.maps.DirectionsStatus.OK) {
+			directionsDisplay.setDirections(response);
+		} else {
+			window.alert('Directions request failed due to ' + status);
+		}
+	});
+}
+
+
+
 
 //FACEBOOK
 window.fbAsyncInit = function() {
