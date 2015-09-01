b9ea40eb1a7a54310e3f4141133ec04650d3e5d3
Avi Zacherman <avizacherman@gmail.com>
2015-09-01 16:28:59 -0400

    Deleting Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
 
 	def delete
-		puts "Referer #{request.referer}"
-		puts "Params #{params}"
+		signed_request = params[:signed_request]
+		encrypted_string = signed_request.split('.')[1]
+		decrypted_id = Base64.decode64(encrypted_string).match('\\"user_id\\":\\"[0-9]+\\"').to_s.match(/[0-9]+/).to_s.to_i
+		user = User.find_by(fb_id: decrypted_id)
+		user.delete
+		render nothing: true
 	end
 
 end
