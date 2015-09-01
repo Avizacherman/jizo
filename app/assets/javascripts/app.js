740e8817bde36fa98e7fac8a737fe8156cf7dd19
Avi Zacherman <avizacherman@gmail.com>
2015-09-01 14:29:21 -0400

    Date formating

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -101,6 +101,15 @@ function initialize() {
 		})
 	})
 
+//Format Dates and Times
+
+$(".start-date-value").each(function(){
+    initialValue = $(this).text()
+    console.log(initialValue)
+    formatedValue = moment(initialValue).format('LL')
+    $(this).text(formatedValue)
+})
+
 	verticalMagic();
 }
 
