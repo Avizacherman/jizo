b40e664116dbae9d19c5bd3336c9b45474ebc871
Avi Zacherman <avizacherman@gmail.com>
2015-09-01 09:54:39 -0400

    Client Side AJAX and DOM Manip

--- app/controllers/directions_controller.rb
+++ app/controllers/directions_controller.rb
@@ -2,14 +2,14 @@ class DirectionsController < ApplicationController
 
 def get_directions
 	
-	if request.xhr? 
+	# if request.xhr? 
 		origin = params[:origin]
 		destination = params[:destination]
 		
 		directions = HTTParty.get("https://maps.googleapis.com/maps/api/directions/json?origin=#{origin}&destination=#{destination}&key=#{ENV['JIZO_MAP_S_KEY']}").parsed_response["routes"][0]["legs"][0]["steps"]
 
 		render json:  {:directions => directions}
-	end
+	# end
 end
 
 end
