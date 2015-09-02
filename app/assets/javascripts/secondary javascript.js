b9937cfe204c477afb075e789502a04ec14739f7
Avi Zacherman <avizacherman@gmail.com>
2015-09-02 18:13:25 -0400

    Fixed settings

--- app/assets/javascripts/secondary javascript.js
+++ app/assets/javascripts/secondary javascript.js
@@ -1,17 +1,14 @@
-$('#settings').click(function(){
-	settings = $.get('/settings/load').done(function(){
-		if(settings) {
-		set = settings.responseJSON.settings[0] 
-		console.log(set)
-		$('input[value=' + set.transportation+ ']').attr('checked', 'true')
-		$('input[value=' + set.location+ ']').attr('checked', 'true')
-		if (set.location === 'enter_location'){
-					$(".text").removeClass("hide");
-					$('.text').val(set.custom_location)
-		}
+	// settings = $.get('/settings/load').done(function(){
+	// 	if(settings) {
+	// 	set = settings.responseJSON.settings[0] 
+	// 	console.log(set)
+	// 	$('input[value=' + set.transportation+ ']').attr('checked', 'true')
+	// 	$('input[value=' + set.location+ ']').attr('checked', 'true')
+	// 	if (set.location === 'enter_location'){
+	// 				$(".text").removeClass("hide");
+	// 				$('.text').val(set.custom_location)
+	// 	}
 
-		$('#settingsModal').foundation('reveal', 'open')
-		}
-	})
-
-})
+	// 	$('#settingsModal').foundation('reveal', 'open')
+	// 	}
+	// })
