43414ef3fb90602290aa84cd98cde286b5a565bf
Avi Zacherman <avizacherman@gmail.com>
2015-09-02 20:17:48 -0400

    asset pipeline

--- Gemfile
+++ Gemfile
@@ -56,5 +56,4 @@ group :development do
   gem 'spring'
 end
 
-gem 'rails_12factor', group: :production
 
--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,5 @@ module Jizo
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    Koala.config.api_version = 'v2.4'
-
   end
 end
