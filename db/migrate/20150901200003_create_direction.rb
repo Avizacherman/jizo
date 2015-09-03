13353e05378b7c7284575a742b56fa3cd6c51a18
augustine mudrak <augustinemudrak@gmail.com>
2015-09-02 22:24:39 -0400

    finished README

--- db/migrate/20150901200003_create_direction.rb
+++ db/migrate/20150901200003_create_direction.rb
@@ -1,7 +1,7 @@
 class CreateDirection < ActiveRecord::Migration
   def change
     create_table :directions do |t|
-      t.string :directions_data
+      t.text :directions_data
       t.integer :user_id
       t.string :origin
       t.string :destination
